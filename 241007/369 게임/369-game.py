3ffa7cadb5f7cf44ca4fbe9e87bf09d415df9c41
youn yeo won <54175366+tripleyoung@users.noreply.github.com>
2024-10-07 16:36:09 +0900

    Title: 369 게임, Time: 114ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241007/369 게임/369-game.py
+++ 241007/369 게임/369-game.py
@@ -2,7 +2,7 @@ n = int(input())
 total_str=""
 for i in range(1,n+1):
     j = str(i)
-    if("3" in j or "6" in j or "9" in j):
+    if("3" in set(j) or "6" in set(j) or "9" in set(j)):
         total_str+="0"
     else:
         total_str+=j
