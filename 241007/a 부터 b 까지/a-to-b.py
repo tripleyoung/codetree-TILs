963ba83a7d2d9ebef4a3521ccb1596c65fbac9b4
youn yeo won <54175366+tripleyoung@users.noreply.github.com>
2024-10-07 15:26:36 +0900

    Title: a 부터 b 까지, Time: 148ms, Memory: 24MB, Status: Passed - CodeTree

--- 241007/a 부터 b 까지/a-to-b.py
+++ 241007/a 부터 b 까지/a-to-b.py
@@ -3,12 +3,14 @@ total_str=""
 i=a
 total_str+=str(i)
 total_str+=" "
-while(i<b):
+while(i<=b):
     if(i%2==0):
         i+=3
-        total_str+=str(i)
+        if(i<=b):
+            total_str+=str(i)
     else:
         i*=2
-        total_str+=str(i)
+        if(i<=b):
+            total_str+=str(i)
     total_str+=" "
 print(total_str)
